5d46249574612d80f39774d4bfef7bafe90cf94a
kei.ebina <93515abk@gmail.com>
2019-06-21 01:44:43 +0900

    add Search System

--- src/main/java/com/example/demo/login/controller/SearchController.java
+++ src/main/java/com/example/demo/login/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.example.demo.login.controller;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,10 +32,15 @@ public class SearchController {
 
 	@Transactional(readOnly = false)
 	@RequestMapping(value = "/search", method = RequestMethod.GET)
-	public ModelAndView getSearch(@RequestParam("search") String searchWord, ModelAndView mav) {
-		srRepository.deleteAll();
-		String[] word = searchWord.split("[ ,　]");
+	public ModelAndView getSearch(@RequestParam("search") String searchWord, ModelAndView mav, Principal principal) {
+		String userId = principal.getName();													//ナビゲーションバー表示用
+		srRepository.deleteAll();																	//テーブル全削除
+		String[] word = searchWord.split("[ ,　]");											//半角・全角スペースで文字列をわける
+		if (searchWord.equals("")) {
+			searchWord = "全商品";
+		}
 		mav.addObject("searchWord", searchWord);
+		//検索ワードで商品をあいまい検索して、データベースに保存
 		for (int i = 0; i < word.length; i++) {
 			List<Product> list = new ArrayList<>();
 			searchWord = "%" + word[i] + "%";
@@ -47,6 +53,13 @@ public class SearchController {
 			}
 		}
 		List<Product> products = daService.getAllSearchResult();
+		boolean searchResultFlag = false;
+		if (products.size() > 0) {
+			searchResultFlag = true;
+		}
+		mav.addObject("searchResultFlag", searchResultFlag);
+		mav.addObject("userId", userId);
+		mav.addObject("in", true);
 		mav.addObject("products", products);
 		mav.addObject("categoryItems", pService.getCategoryItems());			//サイドバー表示アイテム
 		mav.setViewName("layout/layout");
